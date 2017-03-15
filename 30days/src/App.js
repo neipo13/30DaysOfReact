c8226c0cd42441951fc9d218faf67748fb08bc37
Andrew Napierkowski <andrewnapierkowski@Kodys-MBP.fios-router.home>
2017-03-15 18:17:48 -0400

    day 13

---
+++ 30days/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const a = [1, 10, 100, 1000];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <ul>
+          {React.Children.map(a, i => <li>{i}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
